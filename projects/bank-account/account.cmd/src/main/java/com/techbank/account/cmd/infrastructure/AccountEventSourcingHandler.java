59497647eec4091571ad4261006aff020004d856
dimitar <dimitar.vukman@bluedropmedical.com>
2023-02-15 15:03:39 +0100

    Event Sourcing Handler

---
+++ projects/bank-account/account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventSourcingHandler.java
@@ -0,0 +1,36 @@
+package com.techbank.account.cmd.infrastructure;
+
+import com.techbank.account.cmd.domain.AccountAggregate;
+import com.techbank.cqrs.core.domain.AggregateRoot;
+import com.techbank.cqrs.core.events.BaseEvent;
+import com.techbank.cqrs.core.handlers.EventSourcingHandler;
+import com.techbank.cqrs.core.infrastructure.EventStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class AccountEventSourcingHandler implements EventSourcingHandler<AccountAggregate> {
+    @Autowired
+    private EventStore eventStore;
+
+    @Override
+    public void save(AggregateRoot aggregate) {
+        eventStore.saveEvent(aggregate.getId(), aggregate.getUncommittedChanges(), aggregate.getVersion());
+        aggregate.markChangesAsCommitted();
+    }
+
+    @Override
+    public AccountAggregate getById(String id) {
+        AccountAggregate aggregate = new AccountAggregate();
+        List<BaseEvent> events = eventStore.getEvents(id);
+        if (events != null && !events.isEmpty()) {
+            aggregate.replayEvents(events);
+            int latestVersion = events.stream().map(BaseEvent::getVersion).max(Comparator.naturalOrder()).get();
+            aggregate.setVersion(latestVersion);
+        }
+        return aggregate;
+    }
+}
